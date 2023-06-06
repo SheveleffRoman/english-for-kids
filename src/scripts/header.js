6a7759b43e3f74689e48052bcec75b7f830e885a
Roman <@>
2023-06-06 18:35:51 +0300

    feat: add blackout and new colours

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,10 +1,17 @@
 export function toggleMenu() {
     const burger = document.querySelector('div.burger');
     const menu = document.querySelector('ul.menu');
+    const blackout = document.querySelector('div.blackout')
     
     burger.addEventListener('click', () => {
         menu.classList.toggle('show')
         burger.classList.toggle('open')
         document.body.classList.toggle('no-scroll')
+        blackout.classList.toggle('view')
+
+    })
+
+    blackout.addEventListener('click', () => {
+        burger.click()
     })
 }
