11c31da9ed192ff36c46c20ee4232f713138242f
Roman <@>
2023-06-12 20:49:24 +0300

    feat: add game logic
    When the user click the "play" button, a random list of words is generated. The first word is then played and the application waits for the correct card to be pressed. If true, then the next word is played, if an error, then the application will wait for the correct answer. When you click on the "repeat" button, the last word will be played.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { toggleMenu, menuClick } from "./scripts/header.js";
 import { categoryCardClick } from "./scripts/createCategoryCards.js";
-import { playPanelButtonsOn, playPanelView } from "./scripts/interactive.js";
+import { goPlay, playPanelButtonsOn, playPanelView } from "./scripts/interactive.js";
 toggleMenu();
 menuClick();
 categoryCardClick();
 playPanelView();
 playPanelButtonsOn();
+goPlay()
 
 // const changeMode = document.getElementById("app_mode_input");
 // const playBtn = document.querySelector('.start-game');
--- src/scripts/interactive.js
+++ src/scripts/interactive.js
@@ -1,7 +1,7 @@
 import { cards, menuLinks } from "./header.js";
 
-export const playBtn = document.querySelector('.start-game');
-export const repeatBtn = document.querySelector('.repeat-word');
+export const playBtn = document.querySelector(".start-game");
+export const repeatBtn = document.querySelector(".repeat-word");
 
 export function flipButtons() {
   const flipBtn = document.querySelectorAll(".info_btn");
@@ -22,15 +22,15 @@ export function flipButtons() {
 }
 
 export function playSound() {
-    const soundBtn = document.querySelectorAll('.sound_btn');
-    console.log(soundBtn);
-    soundBtn.forEach(btn => {
-        btn.addEventListener('click', function() {
-            let soundPath = btn.firstChild.getAttribute('data-sound')
-            let audio = new Audio(soundPath);
-            audio.play()
-        })
-    })
+  const soundBtn = document.querySelectorAll(".sound_btn");
+  console.log(soundBtn);
+  soundBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let soundPath = btn.firstChild.getAttribute("data-sound");
+      let audio = new Audio(soundPath);
+      audio.play();
+    });
+  });
 }
 
 const changeMode = document.getElementById("app_mode_input");
@@ -68,4 +68,82 @@ export function playPanelButtonsOn() {
       repeatBtn.setAttribute("disabled", "true");
     });
   });
+}
+
+export function goPlay() {
+  repeatBtn.addEventListener("click", repeatLastWord);
+
+  playBtn.addEventListener("click", async () => {
+    const soundArr = await createSound(); // Получаем массив с объектами
+    playNextWord(soundArr);
+  });
+}
+
+let lastWordObj = null; // Переменная для хранения последнего слова
+
+function playNextWord(soundArr) {
+  if (soundArr.length === 0) {
+    console.log("WELL DONE!");
+    return setTimeout(() => window.location.href = '/index.html', 2000); // Массив слов пуст ВОТ ТУТ ДОЛЖЕН БЫТЬ ИТОГ ПО ОЧКАМ!!!
+  }
+
+  let wordObj = soundArr.shift(); // Извлекаем первое слово из массива
+  console.log(wordObj);
+  lastWordObj = wordObj; // Сохраняем текущее слово как последнее
+  let soundPath = wordObj.sound;
+  let audio = new Audio(soundPath);
+  audio.play();
+
+  // Добавляем обработчик клика для каждой карточки
+  const cardElements = document.querySelectorAll(".card_container");
+  cardElements.forEach((card) => {
+    card.addEventListener("click", function clickHandler() {
+      let cardText = card.querySelector(".info_title").textContent;
+      if (cardText === wordObj.en) {
+        card.removeEventListener("click", clickHandler);
+        playNextWord(soundArr); // Проигрываем следующее слово
+      } // тут добавить звук ошибки
+    });
+  });
+}
+
+function repeatLastWord() {
+  if (lastWordObj) {
+    let soundPath = lastWordObj.sound;
+    let audio = new Audio(soundPath);
+    audio.play();
+  }
+}
+
+function createSound() {
+  let url = new URL(window.location.href);
+  let hash = url.hash.slice(1);
+  return getSoundArray(hash);
+}
+
+async function getSoundArray(category) {
+  try {
+    const response = await fetch("./src/scripts/cards.json");
+    const data = await response.json();
+    let soundArr = data[category].words;
+    soundArr = shuffleArray(soundArr); // Перемешиваем массив
+    console.log(soundArr);
+    return soundArr;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function shuffleArray(array) {
+  let currentIndex = array.length;
+  while (currentIndex !== 0) {
+    const randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
 }
