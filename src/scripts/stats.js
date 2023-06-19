056891882a08a19b3a70342e443afc78e0a6094f
Roman <@>
2023-06-19 15:01:23 +0300

    feat: add statistics from locale storage and counting
    when the user clicks on the statistics button, the program counts the number of correct and incorrect answers for each word

--- src/scripts/stats.js
+++ src/scripts/stats.js
@@ -1,3 +1,4 @@
+import { cardsContainer } from "./createCategoryCards.js";
 import { correctWords, incorrectWords } from "./interactive.js";
 
 const statsBtn = document.querySelector(".stats_btn");
@@ -5,43 +6,76 @@ const statsBtn = document.querySelector(".stats_btn");
 export function showStats() {
   statsBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log(localStorage);
+    cardsContainer.innerHTML = "";
+    getStats()
   });
 }
 
 export function pushToLocalStorage() {
-    if (!localStorage.correctWords && !localStorage.incorrectWords) {
-        localStorage.setItem(
-          "correctWords",
-          JSON.stringify(correctWords)
-        );
-        localStorage.setItem(
-          "incorrectWords",
-          JSON.stringify(incorrectWords)
-        );
-      } else {
-        // Получаем текущий массив слов из локального хранилища (если он существует)
-        let existingCorrectWords = localStorage.getItem("correctWords");
-        let existingIncorrectWords =
-          localStorage.getItem("incorrectWords");
+  if (!localStorage.correctWords && !localStorage.incorrectWords) {
+    localStorage.setItem("correctWords", JSON.stringify(correctWords));
+    localStorage.setItem("incorrectWords", JSON.stringify(incorrectWords));
+  } else {
+    // Получаем текущий массив слов из локального хранилища (если он существует)
+    let existingCorrectWords = localStorage.getItem("correctWords");
+    let existingIncorrectWords = localStorage.getItem("incorrectWords");
 
-        // Если уже есть слова в локальном хранилище, преобразуем их из JSON в массив
-        existingCorrectWords = JSON.parse(existingCorrectWords);
-        existingIncorrectWords = JSON.parse(existingIncorrectWords);
+    // Если уже есть слова в локальном хранилище, преобразуем их из JSON в массив
+    existingCorrectWords = JSON.parse(existingCorrectWords);
+    existingIncorrectWords = JSON.parse(existingIncorrectWords);
 
-        // Добавляем новые слова к существующим
-        existingCorrectWords = existingCorrectWords.concat(correctWords);
-        existingIncorrectWords =
-          existingIncorrectWords.concat(incorrectWords);
+    // Добавляем новые слова к существующим
+    existingCorrectWords = existingCorrectWords.concat(correctWords);
+    existingIncorrectWords = existingIncorrectWords.concat(incorrectWords);
 
-        // Сохраняем обновленный массив слов в локальное хранилище
-        localStorage.setItem(
-          "correctWords",
-          JSON.stringify(existingCorrectWords)
-        );
-        localStorage.setItem(
-          "incorrectWords",
-          JSON.stringify(existingIncorrectWords)
-        );
-      }
+    // Сохраняем обновленный массив слов в локальное хранилище
+    localStorage.setItem("correctWords", JSON.stringify(existingCorrectWords));
+    localStorage.setItem(
+      "incorrectWords",
+      JSON.stringify(existingIncorrectWords)
+    );
+  }
+}
+
+function getStats() {
+    let getCorrectWords = localStorage.getItem("correctWords");
+    let getIncorrectWords = localStorage.getItem("incorrectWords");
+
+    getCorrectWords = JSON.parse(getCorrectWords);
+    getIncorrectWords = JSON.parse(getIncorrectWords);
+
+    countStats(getCorrectWords);
+    countStats(getIncorrectWords);
+
+}
+
+function countStats (array) {
+    // Подсчет повторений с помощью reduce
+const wordCounts = array.reduce((counts, obj) => {
+    const { category, word } = obj;
+    const key = `${category}-${word}`;
+  
+    // Увеличение счетчика повторений для данного ключа или инициализация его значением 1
+    counts[key] = (counts[key] || 0) + 1;
+  
+    return counts;
+  }, {});
+  
+  // Преобразование результата в объект для удобного доступа
+  const result = {};
+  for (const key in wordCounts) {
+    const [category, word] = key.split('-');
+  
+    // Проверка, есть ли уже запись для данной категории
+    if (!result[category]) {
+      result[category] = {};
+    }
+  
+    // Запись подсчета повторений для слова в категории
+    result[category][word] = wordCounts[key];
+  }
+  
+  // Вывод результатов
+  console.log(result);
+  return result;
 }
